93e33d666de4a61b93fb76c049cce62b3b9daee4
Ramón Arenal <rarenalgutierrez@gmail.com>
2021-12-09 18:53:31 +0100

    fixed debugging issues + template for tests 🙌

--- solutions/day9.spec.ts
+++ solutions/day9.spec.ts
@@ -3,7 +3,9 @@ import { part1, part2 } from './day9';
 const testInput = [[2,1,9,9,9,4,3,2,1,0], [3,9,8,7,8,9,4,9,2,1], [9,8,5,6,7,8,9,8,9,2], [8,7,6,7,8,9,6,7,8,9], [9,8,9,9,9,6,5,6,7,8]];
 
 it('should solve test input for day 9 part 1', () => {
-  expect(part1(testInput)).toEqual(15);
+  const result = part1(testInput);
+
+  expect(result).toEqual(15);
 });
 
 it('should solve test input for day 9 part 2', () => {
---
+++ solutions/template.spec.ts
@@ -0,0 +1,13 @@
+import { part1, part2 } from './dayX';
+
+const testInput = null;
+
+describe('Day X', () => {
+  it('should solve test input for part 1', () => {
+    expect(part1(testInput)).toEqual(0);
+  });
+
+  it('should solve test input for part 2', () => {
+    expect(part2(testInput)).toEqual(0);
+  });
+});
